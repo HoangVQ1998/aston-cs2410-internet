be6c8b0a9876b421349fbddb2496def7a808e77e
Vinh Hoang <vqhoang1998@aston.ac.uk>
2020-02-07 13:57:42 +0000

    Added Testing on get

--- node_intro/index.js
+++ node_intro/index.js
@@ -1,1 +1,15 @@
 console.log("index.js executing");
+
+var express = require("express"); // Make express equal the express package
+var app = express(); //Call the express variable as a new application
+
+app.get( // Say that if someone call get this function would happen
+  "/", // relative path
+  function(req, res) { // This is asynchronous callback (non-block I/O)
+    res.send("Hello, World!") // Send Hello world message
+  } );
+
+var port = 3000; // 3000  is commonly used for confirmation message
+app.listen(port,function() {
+  console.log("Listening on port" + port);
+});
